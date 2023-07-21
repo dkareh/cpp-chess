fe41af01f33f3be3e8c71b1448f270b73c3cc76b
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2023-07-20 20:56:20 -0400

    Add Zig build system support!
    Now you can use the (objectively better ;) Zig build system to
    configure, build, and install my chess game. Assuming you've installed
    Zig, running the game is as simple as `zig build run`.

---
+++ build.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    // Let the person running `zig build` choose the target to build for.
+    const target = b.standardTargetOptions(.{});
+
+    // Let the person running `zig build` choose the optimization mode.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe_cflags = [_][]const u8{
+        "-std=c++17",
+        "-Wall",
+        "-Wextra",
+        "-pedantic",
+    };
+
+    const exe = b.addExecutable(.{
+        .name = "chess",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibC();
+    exe.linkLibCpp();
+    exe.addIncludePath("include");
+    exe.addCSourceFiles(&.{
+        "src/displays/AsciiDisplay.cpp",
+        "src/displays/LetterDisplay.cpp",
+        "src/displays/TwoLetterDisplay.cpp",
+        "src/Board.cpp",
+        "src/chess960.cpp",
+        "src/Display.cpp",
+        "src/Game.cpp",
+        "src/main.cpp",
+        "src/Menu.cpp",
+        "src/Piece.cpp",
+        "src/safe_ctype.cpp",
+    }, &exe_cflags);
+
+    if (exe.target.getOsTag() == .windows) {
+        exe.addCSourceFile("src/displays/WindowsConsoleDisplay.cpp", &exe_cflags);
+        exe.defineCMacro("CHESS_ON_WINDOWS", null);
+    }
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Play chess");
+    run_step.dependOn(&run_cmd.step);
+}
